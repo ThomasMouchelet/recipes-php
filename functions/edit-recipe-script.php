bc196cef5a916a95ed47f059c4bb322820089b81
thomas mouchelet <mouchelet.thomas@gmail.com>
2020-03-23 16:41:28 +0100

    edit script

--- functions/edit-recipe-script.php
+++ functions/edit-recipe-script.php
@@ -1,12 +1,16 @@
 <?php
 
 session_start();
-var_dump($_SESSION);
-var_dump($_POST);
+require("database.php");
 
-// connect to database
-// prepare UPDATE
-// bindParam
-// execute
+$req = $db->prepare("UPDATE recettes SET author = :author, title = :title, 
+ingredients = :ingredients, steps = :steps, image_url = :image_url 
+WHERE id = :recipe_id");
+$req->bindParam(":author", $_SESSION["pseudo"]);
+$req->bindParam(":title", $_POST["title"]);
+$req->bindParam(":ingredients", $_POST["ingredients"]);
+$req->bindParam(":steps", $_POST["steps"]);
+$req->bindParam(":image_url", $_POST["image_url"]);
+$req->execute();
 
 // header Location
