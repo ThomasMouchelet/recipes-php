6bb9c0efe103277b1069b9d385eee10fe5ee2cb9
thomas mouchelet <mouchelet.thomas@gmail.com>
2020-03-23 15:02:11 +0100

    v2

--- functions/clone.php
+++ functions/clone.php
@@ -1,13 +1,18 @@
 <?php
+require('database.php');
 
-// connect to database
+$req = $db->prepare("SELECT * FROM recettes WHERE id = :recipe_id");
+$req->bindParam(":recipe_id", $_GET["recipe_id"]);
+$req->execute();
 
-// prepare SELECT FROM ....
-// bindParam
-// execute
+$result = $req->fetch(PDO::FETCH_ASSOC);
 
-// fetch
+$req = $db->prepare("INSERT INTO recettes(author, title, ingredients, steps, image_url) VALUE (:author, :title, :ingredients, :steps, :image_url)");
+$req->bindParam(":author", $result["author"]);
+$req->bindParam(":title", $result["title"]);
+$req->bindParam(":ingredients", $result["ingredients"]);
+$req->bindParam(":steps", $result["steps"]);
+$req->bindParam(":image_url", $result["image_url"]);
+$req->execute();
 
-
-// générer une nouvelle recette
-// INSERT 
+header("Location: ../recettes.php");
--- recettes.php
+++ recettes.php
@@ -10,7 +10,6 @@ session_start();
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.1/css/all.min.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <link rel="stylesheet" href="dist/css/style.css">
-
 </head>
 <body>
     <nav>
@@ -25,7 +24,7 @@ session_start();
     <?php
     require("functions/database.php");
                 
-    $req = $db->prepare("SELECT * FROM recettes WHERE author = :author");
+    $req = $db->prepare("SELECT * FROM recettes WHERE author = :author ORDER BY id DESC");
     $req->bindParam(":author", $_SESSION["pseudo"]);
     $req->execute();
 
